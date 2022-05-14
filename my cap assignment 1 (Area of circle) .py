b824b2d7e60ee9a753f3450240028df03a884289
Tanishq <104920248+Tan15hq@users.noreply.github.com>
2022-05-14 20:49:10 +0530

    Update my cap assignment 1 (Area of circle) .py

--- my cap assignment 1 (Area of circle) .py
+++ my cap assignment 1 (Area of circle) .py
@@ -1,5 +1,15 @@
+
+# first task
+
 import math
 r = float(input())
 area = math.pi * (r**2)
 print('The area of the circle with radius ', r, 'is:', area)
 
+# second task
+
+fn= input("The Filename: ")
+
+f = fn.split(".")
+
+print ("Extension of the file is : " + f[-1])
